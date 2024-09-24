7a220f91a35d7d2b4c56ae8e4947b8633af5012e
TamirBenIzhak <tamirb771@gmail.com>
2024-09-24 15:03:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from datetime import datetime
 current_datetime = datetime.now()
-print("Current date and time:", current_datetime)
+day_of_week = current_datetime.strftime("%A")
+print(f"Current date and time: {current_datetime}")
+print(f"Day of the week: {day_of_week}")
